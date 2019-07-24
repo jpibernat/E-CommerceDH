83bc5a943d29a3cba98102dd54f563f4440e2774
sabrina1983 <sabrid.calpe@gmail.com>
2019-07-24 20:56:16 -0300

    Se agrega footer y whatsapp fijo

--- index.php
+++ index.php
@@ -104,6 +104,12 @@
 </section>
 </div>
 </div>
+
+<!-- whatsapp -->
+<div class="btn-whatsapp">
+<a href="whatsapp://send?text= http://localhost/E-CommerceDH-master/" data-action="share/whatsapp/share"><img border="0" src="avatares/whatsapp33.jpg" width="50px" height="50px"></a>
+</div>
+
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
@@ -116,4 +122,31 @@
             }
     });</script> -->
   </body>
+  <!-- Footer -->
+  <footer>
+  <div class="container">
+    <div class="row">
+        <div class="col-xs-5 col-md-5">
+            <h5 class="text-muted lead">CONTACTO:</h5>
+            <h5 class="text-muted">
+            Evergreen Street<br>
+            Springfield, EEUU<br>
+            (011) 4356-6645.<br>
+          </h5>
+        </div>
+        <div class="col-xs-12 col-md-6">
+        <div class="pull-right">
+        <h6 class="text-muted lead">EN LAS REDES
+              <div class="redes-footer">
+                  <a href="https://www.facebook.com/"><img src="avatares/face_logo.png" width="30" alt=""></a>
+                  <a href="https://twitter.com/"><img src= "avatares/twitter_logo_azul.jpg" width="30" alt=""></a>
+                  <a href="https://www.instagram.com/"><img src="avatares/instagram_logo.jpg" width="48" alt=""></a></h6>
+              </div>
+        </div>
+
+    </div>
+    <center><div class="row"> <p class="text-muted small text-right">Mukineros.com @2019. | Todos los derechos reservados. |</p></div></center>
+  </div>
+</div>
+</footer>
 </html>
